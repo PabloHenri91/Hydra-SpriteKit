405835306d11129ad181b776ba7ae686978dfdbd
Pablo Henrique Bertaco <pbertaco@umov.me>
2020-03-11 13:01:10 -0300

    Add Tiled code

---
+++ Hydra/Tiled/TiledLayer.swift
@@ -0,0 +1,17 @@
+//
+//  TileLayer.swift
+//  RPG-SpriteKit
+//
+//  Created by Pablo Henrique Bertaco on 21/02/20.
+//  Copyright © 2020 OrganizationName. All rights reserved.
+//
+
+import SpriteKit
+
+class TiledLayer: NSObject {
+
+    var id = ""
+    var name = ""
+    var width: CGFloat = 0
+    var height: CGFloat = 0
+}
